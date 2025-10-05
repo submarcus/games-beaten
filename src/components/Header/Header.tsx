6e26e8207c870418c59c2a1d84a991763e6f702a
Marcus Coelho <marcusrangelcoelho@gmail.com>
2025-10-05 03:21:56 -0300

    change pfp

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -10,7 +10,7 @@ const Header = ({ games }: HeaderProps) => {
    const totalHours = calculateTotalHours(games);
 
    return (
-      <div className="bg-neutral-950  mb-10 cursor-default">
+      <div className="bg-neutral-950 mb-4 cursor-default">
          <div className="flex justify-center items-center">
             <div className="grid grid-cols-3 gap-4 text-center">
                <div className="invisible sm:visible flex flex-col items-center justify-center border border-neutral-800 bg-neutral-950   p-3">
@@ -19,7 +19,7 @@ const Header = ({ games }: HeaderProps) => {
                </div>
                <a href="https://steamcommunity.com/id/maahlune" target="_blank">
                   <img
-                     src="https://i.pinimg.com/736x/a2/b9/62/a2b962edef6e36a594c52fcbf559976d.jpg"
+                     src="https://i.pinimg.com/736x/c3/fa/56/c3fa56d68922b567d3cb319490634e43.jpg"
                      className="border border-neutral-800 bg-neutral-950   size-20 object-cover"
                   />
                </a>
