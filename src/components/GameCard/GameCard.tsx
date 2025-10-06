fcd5e1a591c296a1942ea9c9028fb723fd1912cb
Marcus Coelho <marcusrangelcoelho@gmail.com>
2025-10-06 12:58:28 -0300

    change card style

--- src/components/GameCard/GameCard.tsx
+++ src/components/GameCard/GameCard.tsx
@@ -5,7 +5,7 @@ const GameCard = (game: Game) => {
 
    return (
       <div
-         className="group border border-neutral-900 bg-neutral-950 p-3 transition-colors hover:border-neutral-800"
+         className="group border border-neutral-900 bg-neutral-950 transition-colors hover:border-neutral-800"
       >
          <div className="relative">
             <img
@@ -15,33 +15,23 @@ const GameCard = (game: Game) => {
             />
 
             {/* Sobreposição */}
-            <div className="absolute inset-0 flex flex-col items-center justify-center  bg-black/70 text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100 *:cursor-default">
-               <span className="text-md font-medium">{game.data}</span>
+            <div className="absolute inset-0 flex flex-col items-center justify-center bg-black/70 text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100 *:cursor-default px-2">
+               <span className="text-xs font-medium text-center line-clamp-2 max-w-full overflow-hidden break-words text-neutral-300">{game.nome}</span>
+               <span className="text-sm font-medium">{game.data}</span>
                <span className="text-sm text-neutral-300">{game.tempo}</span>
+
+               <span
+                  className={`cursor-default absolute right-1 top-1 w-6 h-6 flex items-center justify-center bg-black text-sm font-normal ${getRatingColor(
+                     game.nota
+                  )}`}
+               >
+                  {game.nota}
+               </span>
+
+               <span className="cursor-default absolute bottom-2 left-2 bg-black/80 px-2 py-1 text-xs text-white">
+                  {game.versao}
+               </span>
             </div>
-
-            {/* Nota do Jogo */}
-            <span
-               className={`cursor-default absolute right-1 top-1 w-6 h-6 flex items-center justify-center bg-black text-sm font-normal ${getRatingColor(
-                  game.nota
-               )}`}
-            >
-               {game.nota}
-            </span>
-
-            {/* Versão do Jogo */}
-            <span className="cursor-default absolute bottom-2 left-2 bg-black/80 px-2 py-1 text-xs text-white">
-               {game.versao}
-            </span>
-         </div>
-
-         <div className="mt-2 flex flex-col gap-1 *:cursor-default">
-            <h3 className="truncate text-sm font-medium text-white" title={game.nome}>
-               {game.nome}
-            </h3>
-
-            {/* Gênero */}
-            <span className="w-fit text-xs text-neutral-300">{game.genero[0]}</span>
          </div>
       </div >
    );
